d2fd9362931a78357a17a14fffac9dff93922e7f
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-14 16:46:36 +0200

    add site

---
+++ script.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let section = document.querySelector('section');
+let text = document.querySelector('.text');
+let innerText = document.querySelector('.innerText');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    section.style.clipPath = `circle(${value}px at center center)`;
+    // console.log(value);
+    text.style.left = 100 - value / 5 + `%`;
+    innerText.style.left = 100 - value / 5 + `%`;
+})
